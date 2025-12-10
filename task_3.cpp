61525a8674699ea8a82ec1be6905020099fb9c3b
Ruba Abkam <r.m.d.abkam@gmail.com>
2025-12-10 11:05:22 +0200

    implemnting the inverse function

--- task_3.cpp
+++ task_3.cpp
@@ -1,1 +1,30 @@
-#task3
+
+#include <iostream>
+#include <string>
+using namespace std;
+int find_inverse(int a, int b);
+
+int main()
+{
+    int a, b;
+    cout << "Enter a and b: ";
+    cin >> a >> b;
+    cout << "The inverse of " << a << " modulo " << b << " is: " << find_inverse(a, b) << endl;
+    return 0;
+}
+
+int find_inverse(int a, int b)
+{
+for(int i = 1; i > 0; i++)
+{
+
+    if((i * a) % b == 1)
+    {
+        return i;
+        
+    }
+
+}
+return -1;
+
+}
