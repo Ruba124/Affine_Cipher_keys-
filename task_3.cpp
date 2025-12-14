829fe821a33c1ce157f79994e7dfa7fa85fd460f
Ruba Abkam <r.m.d.abkam@gmail.com>
2025-12-14 19:16:12 +0200

    Enhance Affine Cipher main function to include predefined keys and messages, allowing user interaction for encryption and decryption

--- task_3.cpp
+++ task_3.cpp
@@ -11,12 +11,44 @@ string affine_decrypt(string cipher_text, int a_inv, int b, string alphabet);
 int wrap_a(int a, int m);
 
 int main()
-{ int a, b;
+{ int a, b,a_1, b_1,c;
+int count = 0;
+bool exit = false;
     string active_samon;
   cout << "--- Affine Cipher (Samoan) ---" << endl;
-    cout << "Enter two keys (a b):  " << endl;
-    cin >> a >> b;
-  
+  cout<< "Part A"<<endl;
+  cout<< "The Samoan alphabet has 17 letters (A, E, I, O, U, F, G, H, L, M, N, P, R, S, T, V, K) and an optional space character to use if the keys are not coprime with 17." << endl;
+  cout<< "The message is E TAUA LENEI GALUEGA"<<endl;
+  cout << "The keys are 17, 13" << endl;
+  a_1 = 17;
+  b_1 = 13;
+  while(!exit){
+    if ( count == 0){
+        a = a_1;
+        b = b_1;
+       
+    }
+    else if ( count ==1){
+         cout<< "----------------------------------"<<endl;
+         cout<< "Part B"<<endl;
+         cout<< "Now it's your turn to try! "<<endl;
+         cout << "You can enter your own keys and message to see the encryption and decryption process." << endl;
+         cout << "if u want to exit the program enter 0 ,if u dont  want to exit enter any other number   " << endl;
+         cin >> c;
+            if ( c == 0) {
+                exit = true;
+                break;}
+          cout << "Enter two keys (a b):  " << endl;
+        cin >> a >> b;}
+    else if (count > 1){
+         cout << "if u want to exit the program enter 0 ,if u dont  want to exit enter any other number   " << endl;
+         cin >> c;
+            if ( c == 0) {
+                exit = true;
+                break;}
+        cout << "Enter two keys (a b):  " << endl;
+        cin >> a >> b;
+    }
     if( cal_gcd(a, 17) == 1){
         active_samon = Samon_base;
         cout << "Using Samoan alphabet without space." << endl;
@@ -29,10 +61,16 @@ int main()
         cout << "Key 'a' is not valid. It must be coprime with " << 17 << " or " << 18 << "." << endl;
         return 1;
     }
-    cout << "Enter your message: " << endl;
     string message;
+    if (count == 0)
+    {
+        message = "E TAUA LENEI GALUEGA";
+    }
+    else if (count >= 1){
+    cout << "Enter your message: " << endl;
+   
     cin.ignore();
-    getline(cin, message);
+    getline(cin, message);}
     string encrypted_message =  affine_encrypt(message, wrap_a(a, active_samon.length()), b, active_samon);
     cout << "Encrypted message: " << encrypted_message << endl;
     cout<<"Decryption part "<<endl;
@@ -42,8 +80,20 @@ int main()
         return 1;
     }
     string decrypted_message = affine_decrypt(encrypted_message, mod_inverse, b, active_samon);
-    cout << "Decrypted message: " << decrypted_message << endl;
-return 0;
+   cout << "Decrypted message: " << decrypted_message << endl;
+    count++;
+}
+
+
+
+
+
+
+
+
+
+    return 0;
+    
 
 }
 int wrap_a(int a, int m){
